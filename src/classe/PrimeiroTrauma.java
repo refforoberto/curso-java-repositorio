5272af05267f46f8a0f9c412e73eeddc2f7b207f
Roberto Filho <roberto.filho@senior.com.br>
2020-03-16 12:36:58 -0300

    Primeiro trauma

---
+++ src/classe/PrimeiroTrauma.java
@@ -0,0 +1,17 @@
+package classe;
+
+public class PrimeiroTrauma {
+
+	int a = 2;
+	static int b = 3;
+	
+	public static void main(String[] args) {
+		
+		PrimeiroTrauma p = new PrimeiroTrauma();
+		
+		System.out.println(p.a);
+		System.out.println(b);
+
+	}
+
+}
